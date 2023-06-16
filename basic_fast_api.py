81a87cd1cab69ac719d29c3ba18cbead3ca0f83e
Sanket1202 <sanket1ga19ec142@gmail.com>
2023-06-16 08:39:00 +0530

    basic_fast_api

---
+++ basic_fast_api.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/add/")
+def add(num1: float, num2: float):
+    return {"the add of two num is": num1 + num2}
+
+
+@app.get("/sub/")
+def sub(num1: float, num2: float):
+    return {"the sub of two num is": num1 - num2}
+
+
+@app.get("/mul/")
+def mul(num1: float, num2: float):
+    return {"the multiplication of two num is": num1 * num2}
+
+
+@app.get("/div/")
+def div(num1: float, num2: float):
+    try:
+        return {"The division of two num is": num1 / num2}
+    except Exception as e:
+        print(e)
+
+
+@app.put("/division_update/")
+def div_up(num1: float, num2: float):
+    return{"updated division value is": num1 / num2}
+
+
+if __name__ == "__main__":
+    uvicorn.run("basic_fast_api:app")
